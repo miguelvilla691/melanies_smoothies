c3c255808bf8be8fb75b72465f4f104958bf16ad
miguelvilla691 <120768558+miguelvilla691@users.noreply.github.com>
2026-04-03 11:14:09 -0700

    Integrate nutrition information for smoothie ingredients
    Added functionality to fetch and display nutrition information for selected smoothie ingredients.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 # Import python packages.
 import streamlit as st
 from snowflake.snowpark.functions import col
-
+import requests
 
 # Write directly to the app.
 st.title(f"Customize Your Smoothie! :cup_with_straw:")
@@ -44,8 +44,10 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, ' +name_on_order +'!', icon="✅")
       
-#New section to display smoothiefruit nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+if ingredients_list:
+    ingredients_string = ''
+
+      for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ''
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
