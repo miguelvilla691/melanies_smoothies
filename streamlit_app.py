368abd948c09193564d84563802a3e9567dc516c
miguelvilla691 <120768558+miguelvilla691@users.noreply.github.com>
2026-04-08 13:51:41 -0700

    Comment out Streamlit dataframe and output lines
    Comment out dataframe display and ingredient list output.

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,8 +20,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 
 #Convert the Snowpark dataframe to a Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop
+#st.dataframe(pd_df)
+#st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
@@ -30,13 +30,16 @@ ingredients_list = st.multiselect(
     )
 
 if ingredients_list:
-    st.write(ingredients_list)
-    st.text(ingredients_list)
+#    st.write(ingredients_list)
+#    st.text(ingredients_list)
 
     ingredients_string = ''
     
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The Search value for ', fruit_chosen, ' is ', search_on, '.')
 
     #st.write(ingredients_string)
 
