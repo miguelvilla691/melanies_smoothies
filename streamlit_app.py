03c5a6ae2684e5f77f9827b23114d09b25c79987
miguelvilla691 <120768558+miguelvilla691@users.noreply.github.com>
2026-04-08 13:26:40 -0700

    Remove redundant JSON write for smoothie fruit response
    Removed redundant write of smoothie fruit response JSON.

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,4 +57,3 @@ if ingredients_list:
       st.write(fruit_chosen)
       smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
-      st.write(smoothiefroot_response.json())
