ca4efa1c782adfba652e3115a1845fcd2d06147c
miguelvilla691 <120768558+miguelvilla691@users.noreply.github.com>
2026-04-08 13:28:37 -0700

    Update API request to use search_on_value

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,5 +55,5 @@ if ingredients_list:
       ingredients_string += fruit_chosen + ''
       st.subheader(fruit_chosen + ' Nutrition Information')
       st.write(fruit_chosen)
-      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +fruit_chosen)
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" +search_on_value)
       sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
